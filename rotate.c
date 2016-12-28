ece2344f74276c1a3d0e76f1176538c59ba6d3d7
Khaja Navaz Khan <khajanavaz9696@gmail.com>
2016-12-28 20:41:43 +0530

    create rotate.c
    Program to rotate a 5 digit integer by 'm'times:

---
+++ rotate.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i;int m=0;
+	int test_arr[5];
+	printf("Enter a 5 digit number:\n");
+	for(i=0;i<5;i++)
+	scanf("%d",&test_arr[i]);
+	printf("No. of times to rotate:");
+	scanf("%d",&m);
+	for(i=m;i<5;i++)
+	printf("\n%d",test_arr[i]);
+	for(i=0;i<=m;i++)
+	printf("\n%d",test_arr[i]);
+	return 0;
+}	
