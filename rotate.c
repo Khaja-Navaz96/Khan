a260e072af1ef73d07045870df2a1f4773a03bed
Khaja Navaz Khan <khajanavaz9696@gmail.com>
2016-12-29 19:28:31 +0530

    Update rotate.c

--- rotate.c
+++ rotate.c
@@ -5,12 +5,18 @@ int main()
 	int test_arr[5];
 	printf("Enter a 5 digit number:\n");
 	for(i=0;i<5;i++)
-	scanf("%d",&test_arr[i]);
+	{
+		scanf("%d",&test_arr[i]);
+	}
 	printf("No. of times to rotate:");
 	scanf("%d",&m);
 	for(i=m;i<5;i++)
-	printf("\n%d",test_arr[i]);
+	{
+		printf("\n%d",test_arr[i]);
+	}
 	for(i=0;i<=m;i++)
-	printf("\n%d",test_arr[i]);
+	{
+		printf("\n%d",test_arr[i]);
+	}
 	return 0;
 }	
